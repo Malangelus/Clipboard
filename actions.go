8baa63538f53b71f1e78472615753947804ed2e5
Sebastian Gibbs <sebastian.m.gibbs@gmail.com>
2019-09-03 17:32:28 +0100

    Added value self destruct upon retrieval

--- actions.go
+++ actions.go
@@ -24,24 +24,9 @@ func (c *ActionsController) Retrieve(ctx *app.RetrieveActionsContext) error {
 	// Put your logic here
 
 	res := &app.ClipboardRetrieve{}
-	// c.View(func(txn *badger.Txn) error {
-	// 	record, err := txn.Get([]byte(ctx.GUID.String()))
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	if value, err := record.Value(); err != nil {
-	// 		return err
-	// 	} else {
-	// 		fmt.Println(value)
-	// 		convertedValue := string(value)
-	// 		fmt.Println(convertedValue)
-	// 		fmt.Println(&convertedValue)
-	// 		res.Value = &convertedValue
-	// 	}
-	// 	return nil
-	// })
 	if value := c.Cache[ctx.GUID.String()]; value != "" {
 		res.Value = &value
+		c.Cache[ctx.GUID.String()] = ""
 	}
 	return ctx.OK(res)
 	// ActionsController_Retrieve: end_implement
@@ -54,11 +39,6 @@ func (c *ActionsController) Store(ctx *app.StoreActionsContext) error {
 	// Put your logic here
 	res := &app.ClipboardStore{}
 	guid := uuid.NewV4().String()
-	// c.Update(func(txn *badger.Txn) error {
-	// 	// Your code here…
-	// 	txn.Set([]byte(guid), []byte(ctx.Value))
-	// 	return nil
-	// })
 	c.Cache[guid] = ctx.Value
 	res.GUID = &guid
 	return ctx.OK(res)
